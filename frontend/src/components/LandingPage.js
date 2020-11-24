3d73dc702592260de9c8f2f9939156ed46916635
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-11-24 11:00:48 -0800

    Added Landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,9 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import '../CSS/Landing.css';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing page</h1>
+    <div className='landing'>
+      <div className='main-logo'>
+        <a href='#'>
+          <img
+            src='https://s3-media4.fl.yelpcdn.com/assets/srv0/yelp_styleguide/c3484759c57a/assets/img/logos/logo_desktop_xlarge.png'
+            alt='yelp logo'
+            width='160px'
+            height='80px'
+          />
+        </a>
+      </div>
+      <input
+        type='text'
+        placeholder="biryani, San Jose, Max's"
+        className='field request'
+        name='query'
+        // value={query}
+        // onChange={(e) => setquery(e.target.value)}
+        required
+      />
+      <Link to='#' className='search-button'>
+        Search
+      </Link>
     </div>
   );
 };
