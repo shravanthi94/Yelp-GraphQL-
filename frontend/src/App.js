5df388f978e0ff18b34d56976032737befd7b707
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-11-24 10:52:04 -0800

    Added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
+import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import backendURI from './webConfig';
+
+// Yelp Application Pages
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri: `${backendURI}/graphql`,
+});
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={LandingPage} />
+          <section className='max-container'>
+            <Switch></Switch>
+          </section>
+        </Fragment>
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React, { Fragment } from 'react';
+import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
+import '../CSS/Navbar.css';
+
+const Navbar = () => {
+  const handleLogout = () => {
+    window.localStorage.clear();
+  };
+
+  const guestLinks = (
+    <div className='right-nav'>
+      <Link to='/login' className='header-nav-link'>
+        Log In
+      </Link>
+      <Link to='/signup' className='header-nav-button'>
+        Sign Up
+      </Link>
+    </div>
+  );
+
+  const authLinks = (
+    <div className='right-nav'>
+      {localStorage.usertype === 'restaurant' ? (
+        <Link to='/restaurant/profile' className='header-nav-link'>
+          <i className='fas fa-user'></i> Dashboard
+        </Link>
+      ) : (
+        <Link to='/profile' className='header-nav-link'>
+          <i className='fas fa-user'></i> Profile
+        </Link>
+      )}
+      <a href='/' onClick={handleLogout} className='header-nav-link'>
+        Logout
+      </a>
+    </div>
+  );
+
+  const customerLinks = (
+    <Fragment>
+      <Link to='/customer/orders' className='header-nav-link'>
+        Orders
+      </Link>
+      <Link to='/customer/restaurants' className='header-nav-link'>
+        Restaurants
+      </Link>
+    </Fragment>
+  );
+
+  const restaurantLinks = (
+    <Link to='/restaurant/orders' className='header-nav-link'>
+      Orders
+    </Link>
+  );
+
+  return (
+    <Fragment>
+      <div className='top-nav'>
+        <div className='left-nav'>
+          <Link to='/' className='header-nav-link'>
+            <img src={logo} className='logo' alt='logo-img' />
+          </Link>
+          {localStorage.token &&
+            (localStorage.usertype === 'customer'
+              ? customerLinks
+              : restaurantLinks)}
+        </div>
+        {!localStorage.token ? guestLinks : authLinks}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Navbar;
